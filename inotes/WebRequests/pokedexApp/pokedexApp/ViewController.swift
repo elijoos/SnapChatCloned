b78cd9fc761bf23105fadf6a45cf06e638de7232
COMPUTER <computer@MBA11.local>
2017-05-25 17:37:38 -0600

    first commit for pokedex app

---
+++ inotes/WebRequests/pokedexApp/pokedexApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedexApp
+//
+//  Created by COMPUTER on 5/25/17.
+//  Copyright © 2017 COMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
